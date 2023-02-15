04308a368a36e7f15caa85c2de1701d6c5bfe839
alvaramdani <alvaramdani@yahoo.com>
2023-02-15 22:37:29 +0700

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+
+/*================ TYping  animation ==============  */
+var typed = new Typed(".typing",{
+  strings:["","Web Designer", "Youtober", "Fullsatck javascrif", "Data Science"],
+  typeSpeed:100,
+  BackSpeed:50,
+  loop:true
+
+})
